782db2cbe8ec0f5e0f682a0c16e0b16413098951
PawPaww17 <fwzzyanuar1710@gmail.com>
2026-02-06 15:06:50 +0700

    Menambah Class Wizard, dua object Wizard, dan simulasi battle

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Wizard wizardA = new Wizard("Putri", 5);
+Wizard wizardB = new Wizard("Goblin", 10);
+
+Console.WriteLine("Permainan dimulai...");
+Console.WriteLine("Statistik awal");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA);
+wizardA.Attack(wizardB);
+
+Console.WriteLine("Permainan Selesai...\n");
+Console.WriteLine("Statistik Akhir");
+wizardA.ShowStats();
+wizardB.ShowStats();
+public class Wizard
+{
+    public string Name;
+    public int Energy;
+    public int Damage;
+
+    public Wizard(string name, int damage)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+    }
+    public void ShowStats()
+    {
+        Console.WriteLine($"Name : {Name}");
+        Console.WriteLine($"Energy : {Energy}\n");
+    }
+
+    public void Attack(Wizard enemyObj)
+    {
+        enemyObj.Energy -= Damage;
+        Console.WriteLine($"{Name} Menyerang {enemyObj.Name}");
+        Console.WriteLine($"Sisa energi {enemyObj.Name}: {enemyObj.Energy}");
+    }
+
+}
