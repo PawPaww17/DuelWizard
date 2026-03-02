62b1279b20446771342a38fc9b27fdaedc739207
PawPaww17 <fwzzyanuar1710@gmail.com>
2026-03-02 08:32:32 +0700

    menambahkan_mekanisme_aksi_untuk_pemain

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -6,11 +6,47 @@ Console.WriteLine("Statistik awal");
 wizardA.ShowStats();
 wizardB.ShowStats();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
-wizardA.Attack(wizardB);
+string pilihan;
 
-wizardB.Heal();
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine($"1. {wizardA.Name} menyerang {wizardB.Name}");
+    Console.WriteLine($"2. {wizardB.Name} menyerang {wizardA.Name}");
+    Console.WriteLine($"3. {wizardA.Name} melakukan heal");
+    Console.WriteLine($"4. {wizardB.Name} melakukan heal");
+
+    Console.WriteLine("\nPilihanmu (1/2/3/4): ");
+    pilihan = Console.ReadLine();
+
+    if (pilihan == "1") wizardA.Attack(wizardB);
+    else if (pilihan == "2") wizardB.Attack(wizardA);
+    else if (pilihan == "3") wizardA.Heal();
+    else if (pilihan == "4") wizardB.Heal();
+    else Console.WriteLine("!!! Pilihanmu tidak valid !!!");
+
+    if (wizardA.Energy <= 0 || wizardB.Energy <= 0)
+    {
+        Console.WriteLine("!!! Pertandingan Berakhir !!!");
+        if (wizardA.Energy > wizardB.Energy)
+        {
+            Console.WriteLine($"{wizardB.Name} berhasil dikalahkan");
+            Console.WriteLine($"{wizardA.Name} keluar sebagai pemenangnya!");
+        }
+        else
+        {
+            Console.WriteLine($"{wizardA.Name} berhasil dikalahkan");
+            Console.WriteLine($"{wizardB.Name} keluar sebagai pemenangnya!");
+        }
+        break;
+    }
+    Console.ReadLine();
+}
+
+//wizardA.Attack(wizardB);
+//wizardB.Attack(wizardA);
+//wizardA.Attack(wizardB);
+//wizardB.Heal();
 
 Console.WriteLine("Permainan Selesai...\n");
 Console.WriteLine("Statistik Akhir");
